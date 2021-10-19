2404227a7a0fbbaa1eef6651b3e396ef3d4bc9b7
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-10-19 16:50:05 +0200

    external-api: created skeleton Node.js project

---
+++ external-api/src/middleware/errorHandler.ts
@@ -0,0 +1,20 @@
+import HttpException from './HttpException';
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+  error: HttpException,
+  request: Request,
+  response: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+): undefined => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong';
+
+  response.status(status).send({
+    status: response.statusCode,
+    message: message
+  });
+
+  return undefined;
+};
---
+++ external-api/src/server.ts
@@ -0,0 +1,29 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { router } from './routes/router';
+import { errorHandler } from './middleware/errorHandler';
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  console.log(`no PORT found in process.env`);
+  process.exit(1);
+}
+
+const port: number = parseInt(process.env.PORT as string, 10);
+const basePath = process.env.BASE_PATH as string;
+
+const app: Application = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(basePath, router);
+app.use(errorHandler);
+
+// Server Listening
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+  console.log(`app running here -> http://localhost:${port}`);
+});

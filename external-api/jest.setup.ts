019907944ffc3896a103551f5ff452b6088bc55d
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-11-26 17:11:03 +0100

    Updated .env.example files

--- external-api/jest.setup.ts
+++ external-api/jest.setup.ts
@@ -1,3 +1,4 @@
+// Setup required environment variables
 process.env.BASE_PATH = '/api/v1';
-process.env.AUTH_EMAIL = '123@noemail.com';
-process.env.AUTH_PWD = 'password1';
+process.env.AUTH_EMAIL = 'user123@noemail.com';
+process.env.AUTH_PWD = 'password123';

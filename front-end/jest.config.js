0fc19ba8446bbe9b8eecd1ea182d15e3b9843ec4
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2021-12-17 18:06:19 +0100

    front-end: added initial configuration for Jest coverage

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**', '!**/__tests__/**'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      lines: 1
+    }
+  },
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
     '^.+\\.vue$': 'vue-jest'

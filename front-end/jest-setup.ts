7c578eb4b2c5ac938bfd43bd56bd3d7774b896b3
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-01-13 17:36:01 +0100

    front-end: started unit testing of MeterData component

--- front-end/jest-setup.ts
+++ front-end/jest-setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom';
+
+// Setup required environment variables
+process.env.VUE_APP_BACK_END_API_BASE_URL = 'http://localhost:8080/api/v1';
+process.env.VUE_APP_DEFAULT_SMART_METER_ID =
+  '7eb6cb7a-bd74-4fb3-9503-0867b737c2f6';
---
+++ front-end/src/components/__tests__/MeterData.test.ts
@@ -0,0 +1,28 @@
+//import { render, screen } from '@testing-library/vue';
+import { mocked } from 'ts-jest/utils';
+//import MeterData from '../MeterData.vue';
+import MeterDataSerice from '../../services/MeterDataService';
+import Measurement from '../../types/Measurement';
+
+jest.mock('../../services/MeterDataService');
+const mockedMeterDataSerice = mocked(MeterDataSerice);
+
+const mockMeterDataSericeGetMeasurementsRequest = (
+  measurements: Measurement[]
+): void => {
+  mockedMeterDataSerice.getMeasurements = jest
+    .fn()
+    .mockResolvedValue(measurements);
+};
+
+it('renders sidebar menu', () => {
+  mockMeterDataSericeGetMeasurementsRequest([]);
+
+  /*
+  render(MeterData);
+
+  expect(screen.queryByRole('navigation')).toBeInTheDocument();
+  */
+
+  expect(true).toBeTruthy();
+});

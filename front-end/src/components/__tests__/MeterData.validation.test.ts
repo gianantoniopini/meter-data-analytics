0d07fc84da6334a5984d7fec41a79136cc37b3af
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-02-04 17:22:29 +0100

    front-end: added unit tests related to validation

--- front-end/src/components/__tests__/MeterData.validation.test.ts
+++ front-end/src/components/__tests__/MeterData.validation.test.ts
@@ -1,31 +1,58 @@
 import { fireEvent, render, screen } from '@testing-library/vue';
 import MeterData from '../MeterData.vue';
 
-const setup = async (smartMeterIdFilterValue: string): Promise<HTMLElement> => {
+const setup = (): {
+  smartMeterIdFilter: HTMLElement;
+  applyButton: HTMLElement;
+} => {
   render(MeterData);
 
   const smartMeterIdFilter = screen.getByRole('textbox', {
     name: 'Smart Meter Id:'
   });
+  const applyButton = screen.getByRole('button', {
+    name: 'Apply'
+  });
 
-  await fireEvent.update(smartMeterIdFilter, smartMeterIdFilterValue);
-
-  return smartMeterIdFilter;
+  return { smartMeterIdFilter, applyButton };
 };
 
 describe('if Smart Meter Id filter is cleared', () => {
   it('gives validation error', async () => {
-    const smartMeterIdFilter = await setup('');
+    const { smartMeterIdFilter } = setup();
+
+    await fireEvent.update(smartMeterIdFilter, '');
 
     expect(smartMeterIdFilter.title).toEqual('Value is required');
   });
 
   it('disables Apply button', async () => {
-    await setup('');
-    const applyButton = screen.getByRole('button', {
-      name: 'Apply'
-    });
+    const { smartMeterIdFilter, applyButton } = setup();
+
+    await fireEvent.update(smartMeterIdFilter, '');
 
     expect(applyButton).toBeDisabled();
+  });
+});
+
+describe('if Smart Meter Id filter is updated with a value', () => {
+  const smartMeterIdFilterValue = 'abcd-1234';
+
+  it('does not give validation error', async () => {
+    const { smartMeterIdFilter } = setup();
+
+    await fireEvent.update(smartMeterIdFilter, '');
+    await fireEvent.update(smartMeterIdFilter, smartMeterIdFilterValue);
+
+    expect(smartMeterIdFilter.title).toHaveLength(0);
+  });
+
+  it('enables Apply button', async () => {
+    const { smartMeterIdFilter, applyButton } = setup();
+
+    await fireEvent.update(smartMeterIdFilter, '');
+    await fireEvent.update(smartMeterIdFilter, smartMeterIdFilterValue);
+
+    expect(applyButton).toBeEnabled();
   });
 });

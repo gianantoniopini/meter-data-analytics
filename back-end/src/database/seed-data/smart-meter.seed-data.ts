3c9c07943f69f6c9f08490e069ef8d5b874b83df
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-03-17 15:52:19 +0100

    back-end: added more SmartMeter seed data

--- back-end/src/database/seed-data/smart-meter.seed-data.ts
+++ back-end/src/database/seed-data/smart-meter.seed-data.ts
@@ -5,6 +5,51 @@ const smartMeterSeedData: SmartMeter[] = [
     _id: undefined,
     muid: '09a2bc02-2f88-4d01-ae59-a7f60c4a0dd1',
     name: 'Smart Meter 1'
+  },
+  {
+    _id: undefined,
+    muid: '6bf1a9f8-ff0a-4ad3-a1fb-b33cec85f004',
+    name: 'Smart Meter 2'
+  },
+  {
+    _id: undefined,
+    muid: 'b735654a-2567-4967-be46-5c7c28f77c7e',
+    name: 'Smart Meter 3'
+  },
+  {
+    _id: undefined,
+    muid: '668e9f1b-88a0-425e-8dcc-debec4e0f4b7',
+    name: 'Smart Meter 4'
+  },
+  {
+    _id: undefined,
+    muid: 'f6e2f51b-c49f-4862-a0a5-849e804335a1',
+    name: 'Smart Meter 5'
+  },
+  {
+    _id: undefined,
+    muid: '34a5265b-8d8d-496f-91c4-a65e45cf5488',
+    name: 'Smart Meter 6'
+  },
+  {
+    _id: undefined,
+    muid: '905779b7-8a74-442a-8a59-53916a3eb02f',
+    name: 'Smart Meter 7'
+  },
+  {
+    _id: undefined,
+    muid: '2019904b-24d7-47b8-852b-1ef367f58b4e',
+    name: 'Smart Meter 8'
+  },
+  {
+    _id: undefined,
+    muid: '65bfad79-2d2d-4fe8-b4e4-6950c565b8f2',
+    name: 'Smart Meter 9'
+  },
+  {
+    _id: undefined,
+    muid: '00998a02-69d6-4e83-af54-9a0c07b70cfc',
+    name: 'Smart Meter 10'
   }
 ];
 

1af40520051fec6a9f40f045b1cfdb19448320dd
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2023-05-27 18:00:19 +0200

    front-end-2: fixed issue with extra space on the left

--- front-end-2/src/App.vue
+++ front-end-2/src/App.vue
@@ -3,14 +3,7 @@ import TheNavbar from '@/components/TheNavbar.vue'
 </script>
 
 <template>
-  <!-- navbar -->
   <TheNavbar />
 
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12">
-        <RouterView />
-      </div>
-    </div>
-  </div>
+  <RouterView />
 </template>

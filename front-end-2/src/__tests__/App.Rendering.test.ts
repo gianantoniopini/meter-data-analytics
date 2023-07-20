448831bf9d3dea5b4bc3e39f0438bd3dae973900
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2023-07-20 14:11:46 +0200

    front-end-2: added App.Rendering.test

---
+++ front-end-2/src/__tests__/App.Rendering.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import App from '../App.vue'
+import { setupI18n } from '@/i18n'
+import router from '@/router'
+
+const setup = async () => {
+  const component = mount(App, { global: { plugins: [setupI18n(), router] } })
+
+  await router.isReady()
+
+  return component
+}
+
+describe('App', () => {
+  it('renders Navbar menu', async () => {
+    const wrapper = await setup()
+
+    const navigationLinks = wrapper.findAll('.navbar a')
+    expect(navigationLinks).toHaveLength(4)
+    expect(navigationLinks[0].element.textContent).toContain('Meter Data Analytics')
+    expect(navigationLinks[1].element.textContent).toBe('Home')
+    expect(navigationLinks[2].element.textContent).toBe('Meter Data')
+    expect(navigationLinks[3].element.textContent).toBe('Settings')
+  })
+
+  it('renders welcome message', async () => {
+    const wrapper = await setup()
+
+    const heading = wrapper.get('h1')
+    expect(heading.element.textContent).toBe('Welcome to the Meter Data Analytics app.')
+  })
+})

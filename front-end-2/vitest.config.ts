799c11371c0c7e1672e6a59b6807943fba3974b5
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2023-07-25 14:42:46 +0200

    front-end-2: vitest coverage setup

--- front-end-2/vitest.config.ts
+++ front-end-2/vitest.config.ts
@@ -9,6 +9,9 @@ export default mergeConfig(
   viteConfig({ command: 'build', mode: 'test' }),
   defineConfig({
     test: {
+      coverage: {
+        reportsDirectory: './tests/unit/coverage'
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
